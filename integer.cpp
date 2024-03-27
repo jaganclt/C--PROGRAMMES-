8733f5cdff791ed06ccebc60e7ac20346f98b938
jaganclt <jagan.clt37@gmail.com>
2024-03-27 21:06:28 +0530

    27-03-2024

---
+++ integer.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int i;
+	printf("enter a number");
+	scanf("%d",&i);
+		printf("the number you enterd is %d",i);
+	return 0;
+}
+
---
+++ multiply.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>	
+int main()
+{
+	float a,b,c;
+	printf("enter 1st number:");
+	scanf("%f",&a);
+	printf("enter 2nd number:");
+	scanf("%f",&b);
+	c=a*b;
+	printf("multiple of the numbers is:%f",c);
+}
