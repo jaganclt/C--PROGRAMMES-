1705dbbe85f6b11f8620890cc8889b5b01068734
jaganclt <jagan.clt37@gmail.com>
2024-03-29 12:02:24 +0530

    29-03-2024

---
+++ swap.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("enter first number :");
+	scanf("%d" ,&a);
+	 printf("enter second number :");
+	 scanf("%d" , &b);
+	 c=a;
+	 a=b;
+	 b=c;
+	 printf("After swap a=%d, b=%d \n ",a,b);
+	 return 0;
+	 
+}
